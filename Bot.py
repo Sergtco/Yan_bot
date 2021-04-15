ea1274d5d5a2df628253f7b622ef75b5490d484a
Sergtco <75071620+Sergtco@users.noreply.github.com>
2021-04-15 16:31:04 +0200

    Yan_bot 0.0.3b Now forever on;line

--- Bot.py
+++ Bot.py
@@ -49,10 +49,8 @@ def main():
     dp.add_handler(CommandHandler('time', time))
     dp.add_handler(text_handler)
     # Запускаем цикл приема и обработки сообщений.
-    updater.bot.setWebhook('https://yan-bot-sergt.herokuapp.com/' + TOKEN)
-    updater.start_webhook(listen="0.0.0.0",
-                          port=PORT,
-                          url_path=TOKEN)
+    updater.start_polling()
+
     # Ждём завершения приложения.
     # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
     updater.idle()
